6a0c466f8653b21f2f08264daf8a0726d9687bfe
RyanGian <ryan1gian@gmail.com>
2024-09-22 17:07:43 +1000

    Revamped sliding sidebar. Slowly building collapsible mechnanic

--- techlector/src/workstation/Resizable.jsx
+++ techlector/src/workstation/Resizable.jsx
@@ -7,9 +7,34 @@ import Right from "./right.svg?react";
 import Top from "./top.svg?react";
 import { Button } from "@mantine/core";
 import ItemMenu from "./ItemMenu.jsx";
+import {
+  TbLayoutSidebarLeftCollapseFilled,
+  TbLayoutSidebarRightCollapseFilled,
+} from "react-icons/tb";
 
 export default function Resizable() {
-  return <div className="panel-container"></div>;
+  return (
+    <div className="panel-container">
+      <PanelGroup autoSaveId="horizontal" direction="horizontal">
+        <Panel
+          className="panel-left"
+          defaultSize={30}
+          minSize={20}
+          maxSize={50}
+        >
+          <div className="collapsible-navbar">
+            <TbLayoutSidebarLeftCollapseFilled className="left-collapse-button" />
+          </div>
+        </Panel>
+        <PanelResizeHandle className="vertical-resize-bar">
+          <div className="vertical-bar"></div>
+        </PanelResizeHandle>
+        <Panel className="panel-right" defaultSize={30} minSize={20}>
+          right
+        </Panel>
+      </PanelGroup>
+    </div>
+  );
 }
 
 {
@@ -28,9 +53,6 @@ export default function Resizable() {
         <Panel defaultSize={70}>
           <div className="panel-right"></div>
         </Panel> */
-}
-
-{
   /*
         <Panel maxSize={10}>
           <PanelGroup autoSaveId="vertical" direction="vertical">
